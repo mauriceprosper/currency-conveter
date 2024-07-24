b3c3ffcceda1723c58ba728f05dabc0f60150dcf
mauriceprosper <145440627+mauriceprosper@users.noreply.github.com>
2024-07-24 15:48:05 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
-import 'package:currency_converter/currency_converter_cupertino_page.dart';
-import 'package:currency_converter/currency_converter_material_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'currency_converter_cupertino_page.dart';
+import 'currency_converter_material_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyCupertinoApp());
 }
-
-// Types of Widgets
-// 1. StatelessWidget
-// 2. StatefulWidget
-
-// State
-
-// 1. Material Design
-// 2. Cupertino Design
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
